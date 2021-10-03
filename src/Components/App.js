d868d814667a2292cf48d315174296af4a7c5f51
frandrew90 <frandrewjs@gmail.com>
2021-10-03 14:23:26 +0300

    made HW 3 task#1

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,12 +7,7 @@ import s from '../Components/App.module.css';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
 
     filter: '',
   };
@@ -46,6 +41,19 @@ class App extends Component {
     this.state.contacts.some(
       contact => contact.name.toLowerCase() === name.toLowerCase(),
     );
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     return (
